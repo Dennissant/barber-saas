0e6b23a25c8a31c85d955246fcb07cd373151a64
Magnuz-Gs <magno.mgn420@gmail.com>
2026-09-24 20:56:24 -0300

    cria componente footer com tailwind

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,15 +1,12 @@
-import './Footer.css';
 function Footer() {
     return (
-        <footer>
+        <footer className="text-center">
             <p>© 2026 BarberSaaS. Todos os direitos reservados.</p>
-            <section className="links-suporte">
-                <ul>
+                <ul className="flex gap-5 list-none justify-center">
                     <li><a href="/termos">Termos de Serviço</a></li>
                     <li><a href="/privacidade">Política de Privacidade</a></li>
                     <li><a href="/contato">Contato</a></li>
                 </ul>
-            </section>
         </footer>
     );
 }
