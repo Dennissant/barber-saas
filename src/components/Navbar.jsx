9681b2bf0558f5fa1b8b153a48b3b49facb03152
Dennissant <dennysedu50@gmail.com>
2026-09-24 14:03:36 -0300

    feat: cria componente navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+export default function Navbar() {
+  return (
+    <header className="navbar-container">
+      <h2>BarberSaaS</h2>
+      <nav>
+        <ul>
+          <li>Serviços</li>
+          <li>Preços</li>
+          <li>Login</li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
