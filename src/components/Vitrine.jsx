b0de5c88ab38d00e360c9a370968197e3be06d52
M4GN0 <magno.mgn420@gmail.com>
2026-09-23 23:33:38 -0300

    adiciona responsividade aos cards com media query

--- src/components/Vitrine.jsx
+++ src/components/Vitrine.jsx
@@ -16,21 +16,21 @@ function Vitrine() {
           <div className="card">
             <h3>Corte Cabelo</h3>
             <p>Corte mais lavagem.</p>
-            <span>R$ 50,00 reais</span>
+            <span>R$50,00 reais</span>
             <button>Agendar</button>
           </div>
 
           <div className="card">
             <h3>Barba</h3>
             <p>Modelagem e corte.</p>
-            <span>R$ 30,00 reais</span>
+            <span>R$30,00 reais</span>
             <button>Agendar</button>
           </div>
 
           <div className="card">
             <h3>Corte Cabelo + Barba</h3>
             <p>Corte de cabelo e modelagem de barba.</p>
-            <span>R$ 70,00 reais</span>
+            <span>R$70,00 reais</span>
             <button>Agendar</button>
           </div>
 
