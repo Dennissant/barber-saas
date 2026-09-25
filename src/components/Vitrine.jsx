ad51cc5e673b602522886994987c62323d8c269f
Magnuz-Gs <magno.mgn420@gmail.com>
2026-09-24 21:18:25 -0300

    converte estilos da vitrine para tailwind

--- src/components/Vitrine.jsx
+++ src/components/Vitrine.jsx
@@ -1,4 +1,3 @@
-import './Vitrine.css';
 function Vitrine() {
   return (
     <main>
@@ -11,23 +10,23 @@ function Vitrine() {
 
       <section className="planos" id="planos">
 
-        <div className="cards">
+        <div className="flex flex-col md:flex-row gap-5 justify-center">
 
-          <div className="card">
+          <div className="p-5 border border-[rgb(193,177,177)] rounded-lg text-center">
             <h3>Corte Cabelo</h3>
             <p>Corte mais lavagem.</p>
             <span>R$50,00 reais</span>
             <button>Agendar</button>
           </div>
 
-          <div className="card">
+          <div className="p-5 border border-[rgb(193,177,177)] rounded-lg text-center">
             <h3>Barba</h3>
             <p>Modelagem e corte.</p>
             <span>R$30,00 reais</span>
             <button>Agendar</button>
           </div>
 
-          <div className="card">
+          <div className="p-5 border border-[rgb(193,177,177)] rounded-lg text-center">
             <h3>Corte Cabelo + Barba</h3>
             <p>Corte de cabelo e modelagem de barba.</p>
             <span>R$70,00 reais</span>
